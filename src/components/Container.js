6b212457bafcc74d46081caeeecf3e3eac455c4e
davide <davide.iaiunese@gmail.com>
2018-07-20 22:02:02 +0200

    Fixed Container code

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,4 @@
-/*import React, { Component } from 'react';
+import React, { Component } from 'react';
 import {GoogleApiWrapper, Map} from 'google-maps-react';
 
 const API_KEY = 'AIzaSyCzE5nhb6F0Tcr_BALpCtN5h_G3onbDdJE';
@@ -38,36 +38,4 @@ export class Container extends Component {
   
   export default GoogleApiWrapper({
     apiKey: (API_KEY)
-  })(Container)*/
-  
-
-  import React, {Component} from 'react'
-  import {Map, Marker, GoogleApiWrapper} from 'google-maps-react'
-  
-  const API_KEY = 'AIzaSyCzE5nhb6F0Tcr_BALpCtN5h_G3onbDdJE';
-
-  export class Container extends Component {
-  
-    render() {
-      return (
-        <div>
-        <Map google={window.google}
-            style={{width: '90%', height: '90%'}}
-            className={'map'}
-            zoom={14}
-            containerStyle={{}}
-            centerAroundCurrentLocation={true}
-            >
-          <Marker
-            name={'SOMA'}
-            position={{lat: 37.778519, lng: -122.405640}} />
-        </Map>
-        </div>
-      )
-    }
-  }
-  
-  export default GoogleApiWrapper({
-    apiKey: API_KEY,
-    libraries: ['places','visualization']
   })(Container)
